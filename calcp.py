fee2d1c67ac42633efe15b7462ecd11f940b4eb6
Vinod Guleria <vinod.guleria@gmail.com>
2018-03-20 01:21:39 +0000

    changed the input file name and made input timestamp mandatory

--- calcp.py
+++ calcp.py
@@ -6,18 +6,17 @@ import argparse
 def getCount(inptime):
     count=0
     inptime=datetime.datetime.strptime(inptime,'%H:%M:%S')
-    with open('sample.txt','r') as f:
+    with open('input_calcp.txt','r') as f:
         for line in f:
             intime,extime=(datetime.datetime.strptime(x,'%H:%M:%S') for x in line.split())
-            print("input time is {0} and exittime is {1}".format(intime,extime))
+            #print("input time is {0} and exittime is {1}".format(intime,extime))
             if inptime >= intime and inptime <= extime:
                 count += 1
     return count
 
 ap=argparse.ArgumentParser(description='Get the time during which the empolyee count is required')
 ap.add_argument('-p','--persons',dest='persons')
-ap.add_argument('-t','--time',dest='timestamp')
+ap.add_argument('-t','--time',dest='timestamp',required=True)
 results=ap.parse_args()
-#print(results)
 pers=getCount(results.timestamp)
 print("The total no. of persons present during {0} are: {1}".format(results.timestamp,pers))
